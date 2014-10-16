65a3ec879e268eddeec978f8201db855ff1ce7b3
C2056 <eric.mourgaya-virapatrin@arkea.com>
2014-10-16 21:54:05 +0200

    backup crushmap

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Description
+-----------
+
+Contains scripts  used for ease administration of ceph
+
+SCRIPTS
+-----------
+
+crush_backup  :   backup crushmap and keep the last seven days backup files
---
+++ crush_backup/savcrushmap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#eric.mourgaya@gmail.com
+# this script is ruling the  apache Licence.
+# keep the last seven backup
+#crontab
+#0 7 * * * $0 
+# be careful we need  a serveur with  ceph.conf and key to do this
+
+crushbinfile="crusmap"
+SUFFIX=`date +'%d%m%Y'`
+BASEDIR=/opt/crushmap_sink
+
+mkdir -pv $BASEDIR/bin
+mkdir -pv $BASEDIR/txt
+
+# extract the current crushmap
+
+ceph osd getcrushmap -o $BASEDIR/bin/$crushbinfile.bin.SUFFIX
+crushtool -d $BASEDIR/bin/$crushbinfile.SUFIX -o $BASEDIR/bin/$crushbinfile.txt
+
+find $BASEDIR -type f -ctime +7 -name $crushbinfile* -exec rm {} \;
+
+ 
+
